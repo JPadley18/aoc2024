010b38c32e5a1114dad54f7a290debf8cfd07171
Jacob Padley <jacob5180@hotmail.co.uk>
2024-12-10 22:13:14 +0000

    Add solutions for Day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -3,5 +3,129 @@ use crate::days::PuzzleSolver;
 pub struct Day10;
 
 impl PuzzleSolver for Day10 {
+  fn solve_part_1(&self, input: &str) -> String {
+    // Store the map as a vector for later traversal
+    let mut map: Vec<Vec<char>> = Vec::new();
+    let mut trailheads: Vec<(usize, usize)> = Vec::new();
+    for (y, line) in input.lines().enumerate() {
+      map.push(line.chars().collect());
+      // Store any positions of zeros (trailheads)
+      for (x, chr) in line.chars().enumerate() {
+        if chr == '0' {
+          trailheads.push((x, y));
+        }
+      }
+    }
 
+    // Iterate over the trailheads and score them
+    let mut score = 0;
+    for trailhead in trailheads {
+      // Score the trailhead
+      score += Day10::score_subtree(trailhead, &map, &mut Vec::new());
+    }
+
+    score.to_string()
+  }
+
+  fn solve_part_2(&self, input: &str) -> String {
+    // Store the map as a vector for later traversal
+    let mut map: Vec<Vec<char>> = Vec::new();
+    let mut trailheads: Vec<(usize, usize)> = Vec::new();
+    for (y, line) in input.lines().enumerate() {
+      map.push(line.chars().collect());
+      // Store any positions of zeros (trailheads)
+      for (x, chr) in line.chars().enumerate() {
+        if chr == '0' {
+          trailheads.push((x, y));
+        }
+      }
+    }
+
+    // Iterate over the trailheads and score them
+    let mut score = 0;
+    for trailhead in trailheads {
+      // Score the trailhead
+      score += Day10::score_subtree_part_2(trailhead, &map, &mut Vec::new());
+    }
+
+    score.to_string()
+  }
+}
+
+impl Day10 {
+  fn score_subtree(trailhead: (usize, usize), map: &Vec<Vec<char>>, visited: &mut Vec<(usize, usize)>) -> i32 {
+    visited.push(trailhead);
+    let this_space = Day10::get_space((trailhead.0 as i32, trailhead.1 as i32), map).unwrap();
+    if this_space == 9 {
+      // Base case
+      return 1
+    } else {
+      // Return the sum of all the neighbouring spaces
+      let mut score = 0;
+      for neighbour in Day10::get_neighbours((trailhead.0 as i32, trailhead.1 as i32)) {
+        // Check if neighbour is visitable
+        if neighbour.0 >= 0 && neighbour.1 >= 0 {
+          if !visited.contains(&(neighbour.0 as usize, neighbour.1 as usize)) {
+            let neighbour_space = Day10::get_space(neighbour, map);
+            if neighbour_space.is_some() {
+              if neighbour_space.unwrap() == this_space + 1 {
+                score += Day10::score_subtree((neighbour.0 as usize, neighbour.1 as usize), map, visited);
+              }
+            }
+          }
+        }
+      }
+      score
+    }
+  }
+
+  fn score_subtree_part_2(trailhead: (usize, usize), map: &Vec<Vec<char>>, visited: &mut Vec<(usize, usize)>) -> i32 {
+    let this_space = Day10::get_space((trailhead.0 as i32, trailhead.1 as i32), map).unwrap();
+    if this_space == 9 {
+      // Base case
+      return 1
+    } else {
+      // Return the sum of all the neighbouring spaces
+      let mut score = 0;
+      for neighbour in Day10::get_neighbours((trailhead.0 as i32, trailhead.1 as i32)) {
+        // Check if neighbour is visitable
+        if neighbour.0 >= 0 && neighbour.1 >= 0 {
+          if !visited.contains(&(neighbour.0 as usize, neighbour.1 as usize)) {
+            let neighbour_space = Day10::get_space(neighbour, map);
+            if neighbour_space.is_some() {
+              if neighbour_space.unwrap() == this_space + 1 {
+                // Create a new visited list for the subtree to use
+                let mut new_visited = visited.clone();
+                new_visited.push(trailhead);
+                score += Day10::score_subtree_part_2((neighbour.0 as usize, neighbour.1 as usize), map, &mut new_visited);
+              }
+            }
+          }
+        }
+      }
+      score
+    }
+  }
+
+  fn get_space(pos: (i32, i32), map: &Vec<Vec<char>>) -> Option<u32> {
+    if pos.0 < 0 || pos.1 < 0 {
+      return None
+    }
+    match map.get(pos.1 as usize) {
+      Some(line) => match line.get(pos.0 as usize) {
+        Some(space) => space.to_digit(10),
+        None => None,
+      },
+      None => None,
+    }
+  }
+
+  fn get_neighbours(pos: (i32, i32)) -> Vec<(i32, i32)> {
+    let mut neighbours: Vec<(i32, i32)> = Vec::new();
+    for i in [-1, 1] {
+      neighbours.push((pos.0 + i, pos.1));
+      neighbours.push((pos.0, pos.1 + i));
+    }
+    neighbours
+  }
 }
--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,7 +1,47 @@
 use crate::days::PuzzleSolver;
+use std::collections::HashMap;
 
 pub struct Day8;
 
 impl PuzzleSolver for Day8 {
+  fn solve_part_1(&self, input: &str) -> String {
+    let mut antinodes: Vec<(usize, usize)> = Vec::new();
+    let (node_sets, map_bounds) = Day8::parse_node_lists(input);
+    for node_set in node_sets.values() {
+      // Iterate over each pair and add the antinodes to the resulting list
+      for (i, node) in node_set.iter().enumerate() {
+        // Create an iterator that skips the one being checked
+        let skip_current = node_set.iter().take(i).chain(node_set.iter().skip(i + 1));
+        for other_node in skip_current {
+          // Add the calculated antinode if it doesn't already exist
+          let antinode = (node.0 as i32 + ((other_node.0 as i32 - node.0 as i32) * 2), node.1 as i32 + ((other_node.1 as i32 - node.1 as i32) * 2));
+          if antinode.0 <= map_bounds.0 as i32 && antinode.1 <= map_bounds.1 as i32 && antinode.0 >= 0 && antinode.1 >= 0 {
+            let antinode_as_usize = (antinode.0 as usize, antinode.1 as usize);
+            if !antinodes.contains(&antinode_as_usize) {
+              dbg!(node);
+              dbg!(other_node);
+              antinodes.push(dbg!(antinode_as_usize));
+            }
+          }
+        }
+      }
+    }
+    antinodes.len().to_string()
+  }
+}
 
+impl Day8 {
+  fn parse_node_lists(input: &str) -> (HashMap<char, Vec<(usize, usize)>>, (usize, usize)) {
+    let mut node_lists: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    for (y, line) in input.lines().enumerate() {
+      for (x, char) in line.chars().enumerate() {
+        if char != '.' {
+          // Insert the node into the HashMap of its 'frequency'
+          node_lists.entry(char).or_insert_with(|| Vec::new()).push((x, y));
+        }
+      }
+    }
+    let map_bounds = (node_lists.values().next().unwrap().len(), node_lists.values().len());
+    (node_lists, map_bounds)
+  }
 }
